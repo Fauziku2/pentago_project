79e2c30f2a8860888b68b3597bcff4788cce5442
zhihongchensg <zhihongchensg@gmail.com>
2016-11-10 11:59:50 +0000

    chat 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+
+  get 'chat_rooms/new'
+
+  get 'chat_rooms/create'
+
   devise_for :users, path: '', path_names: {
   sign_in: 'login',
   sign_out: 'logout',
