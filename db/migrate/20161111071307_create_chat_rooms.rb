885ac18e82439d4395a15612ae6e10cd9c9832b6
terence lim <terencelim92@gmail.com>
2016-11-15 17:03:10 +0800

    instant create button

--- db/migrate/20161111071307_create_chat_rooms.rb
+++ db/migrate/20161111071307_create_chat_rooms.rb
@@ -1,7 +1,6 @@
 class CreateChatRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :chat_rooms do |t|
-      t.string :title
       t.references :match, foreign_key: true
 
       t.timestamps
