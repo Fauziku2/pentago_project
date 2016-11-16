700050749f14242bdabd31752d3ad966ae941fd0
fauzi mohd <M.fauzi_87@hotmail.com>
2016-11-16 15:57:09 +0800

    i need a break

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def name
-    email.split('@')[0]
-  end
+  # def name
+  #   email.split('@')[0]
+  # end
 end
